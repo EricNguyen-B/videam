b39db9180c735fd7d459f370c352f7618ddb3e13
Eric Nguyen <ebn26@drexel.edu>
2024-11-02 09:50:48 -0400

    initial commit

---
+++ ViewModels/TimelineViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace Videam.ViewModels
+{
+    public class TimelineViewModel : ViewModelBase
+    {
+        private string? _sceneName;
+        private ObservableCollection<SceneViewModel> _allScenes = new ObservableCollection<SceneViewModel>();
+        
+        //public ICommand AddSceneCommand { get; }
+
+        public string? SceneName
+        {
+            get
+            {
+                return _sceneName;
+            }
+            set
+            {
+                _sceneName = value;
+                OnPropertyChanged(nameof(SceneName));
+            }
+        }
+
+        public ObservableCollection<SceneViewModel> AllScenes
+        {
+            get
+            {
+                return _allScenes;
+            }
+            set
+            {
+                _allScenes = value;
+                OnPropertyChanged(nameof(AllScenes));
+            }
+        }
+    }
+
+    class Updater : ICommand
+    {
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value;}
+        }
+
+        public void Execute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Views/TimelineView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using Videam.ViewModels;
+
+namespace Videam.Views
+{
+    public partial class TimelineView : UserControl
+    {
+        public TimelineView()
+        {
+            InitializeComponent();
+        }
+    }
+} 
